94232fb4c5b09b5e46256d2717681033df079739
Sumi-Wilson <sumikokkadan@gmail.com>
2023-11-22 10:38:41 -0600

    Commiting the first case study files

---
+++ casestudty/src/main/java/homework/casestudty/CasestudtyApplication.java
@@ -0,0 +1,13 @@
+package homework.casestudty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudtyApplication.class, args);
+	}
+
+}
---
+++ casestudty/src/test/java/homework/casestudty/CasestudtyApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.casestudty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasestudtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
